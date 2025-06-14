07c69d6e52f3ff5cf28580ae72cdf79d0b065811
Screameow <hgfm20@gmail.com>
2025-06-13 18:54:55 -0600

    Agrega etiquetas para la identificación de la DB en la clase Usuarios; clases controller para el registro del usuario y configuración de Spring Security

--- backend/src/main/java/com/skilllink/backend/entity/Usuario.java
+++ backend/src/main/java/com/skilllink/backend/entity/Usuario.java
@@ -1,7 +1,21 @@
 package com.skilllink.backend.entity;
 
+import com.skilllink.backend.dto.UsuarioInfRegistro;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Table(name = "usuario")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "idUsuario")
 public class Usuario {
     private Long idUsuario;
     private String nombre;
@@ -9,4 +23,17 @@ public class Usuario {
     private String contrasena;
     private String rol;
     private LocalDateTime fechaRegistro;
+
+    public Usuario (UsuarioInfRegistro usuarioInfRegistro){
+        this.idUsuario = usuarioInfRegistro.idUsuario();
+        this.nombre = usuarioInfRegistro.nombre();
+        this.email = usuarioInfRegistro.email();
+        this.contrasena = usuarioInfRegistro.email();
+        this.rol = usuarioInfRegistro.rol();
+        this.fechaRegistro = LocalDateTime.now();
+    }
+
 }
+
+
+
