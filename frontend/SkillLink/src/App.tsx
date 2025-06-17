c2268e6b70c0e359222d5bf920cd60652d4c91a8
RangerLeonardo <brandonleonardobarrera@gmail.com>
2025-06-16 20:23:37 -0600

    Agregado Router

--- frontend/SkillLink/src/App.tsx
+++ frontend/SkillLink/src/App.tsx
@@ -1,20 +1,24 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import './App.css'
 
 function App() {
 
   return (
-    <>
+    <Router>
       <Navbar />
       <main className="container mx-auto p-4">
-        
-        <h1 className="text-2xl font-bold mb-4">Bienvenidos</h1>
-        <p>Equipo Jedi</p>
+        <Routes>
+          <Route path="/" element={<h1 className="text-2xl">Home Page</h1>} />
+          
+        </Routes>
+
       </main>
       <Footer />
-    </>
+    </Router>
+    
   );
 }
 
-export default App
+export default App;
