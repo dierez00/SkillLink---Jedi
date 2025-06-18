66f73b13a6bed72882b15404936d2440cf1ba1aa
Se-Basstian <p.sebastian.23.11.99@gmail.com>
2025-06-18 16:06:28 -0500

    Se crea Footer como component de diseño

---
+++ frontend/SkillLink/src/components/layout/Footer.tsx
@@ -0,0 +1,41 @@
+import { cn } from "clsx-for-tailwind";
+import MainWidth from "./MainWidth";
+
+const Footer = () => {
+  return (
+    <footer
+      className={cn(
+        "h-[250px] bg-[#7C7D7A] py-10",
+        "shadow-[0_-1px_8px_black]",
+      )}
+    >
+      <MainWidth
+        className={cn(
+          "flex justify-between flex-wrap items-start",
+          "[&>*]:text-white",
+        )}
+      >
+        <p className={cn("text-xl")}>
+          Proyecto creato por el equipo “Jedi” para la AlumniThon
+        </p>
+
+        <div>
+          <h4 className={cn("font-bold mb-3")}>Frond-End</h4>
+
+          <p>Nombre</p>
+          <p>Nombre</p>
+          <p>Nombre</p>
+        </div>
+
+        <div>
+          <h4 className={cn("font-bold mb-3")}>Back-End</h4>
+
+          <p>Nombre</p>
+          <p>Nombre</p>
+        </div>
+      </MainWidth>
+    </footer>
+  );
+};
+
+export default Footer;
