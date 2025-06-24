1c00b28eaaba47173ccb47fb8fcebe5f4617c595
Screameow <hgfm20@gmail.com>
2025-06-24 04:22:22 -0600

    Agregado de Crud a Usuario, perfiles y perfil-habilidad

--- backend/src/main/java/com/skilllink/backend/controller/RecomendacionesController.java
+++ backend/src/main/java/com/skilllink/backend/controller/RecomendacionesController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping ("/api/recomendacion-social")
+@RequestMapping ("api/recomendacion-social")
 public class RecomendacionesController {
 
     @Autowired
