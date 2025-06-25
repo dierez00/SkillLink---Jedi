e911deb0b949e672f9fd335d8d94efe0c09b6e47
Se-Basstian <p.sebastian.23.11.99@gmail.com>
2025-06-25 17:15:52 -0500

    Se agreg react-hot-toast and phosphor-icons como dependencia

--- frontend/SkillLink/src/App.tsx
+++ frontend/SkillLink/src/App.tsx
@@ -1,14 +1,14 @@
+import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
 import DefaultLayout from "./components/common/DefaultLayout";
+import ProtectedRoute from "./components/features/auth/ProtectedRoute";
+import CourseDetails from "./components/features/courses/CourseDetails";
+import CourseLesson from "./components/features/courses/CourseLesson";
+import MyLearning from "./components/features/learning/MyLearning";
+import { CoursesPage } from "./pages/CoursesPage";
 import CreateAcount from "./pages/CreateAcount";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import MyLearning from "./components/features/learning/MyLearning";
-import CourseDetails from "./components/features/courses/CourseDetails";
-import CourseLesson from "./components/features/courses/CourseLesson";
-import { CoursesPage } from "./pages/CoursesPage";
-import { Toaster } from "react-hot-toast";
-import ProtectedRoute from "./components/features/auth/ProtectedRoute";
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/create-account" element={<CreateAcount />} />
-          
+
           {/* Rutas protegidas - solo accesibles con autenticación */}
           <Route element={<ProtectedRoute />}>
             <Route path="/my-learning" element={<MyLearning />} />
@@ -28,15 +28,15 @@ function App() {
           </Route>
         </Route>
       </Routes>
-      
+
       {/* Configuración de notificaciones */}
       <Toaster
         position="top-center"
         toastOptions={{
           duration: 3000,
           style: {
-            background: '#363636',
-            color: '#fff',
+            background: "#363636",
+            color: "#fff",
           },
         }}
       />
